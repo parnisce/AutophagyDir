de4312820802c8c8be9e4e8c62606f316afeb5f7
Cyryl Bitangcol <cyryl.bitangcol@gmail.com>
2026-01-21 01:51:00 +0800

    Remove unused Navigate import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import { Sidebar } from './components/Sidebar';
